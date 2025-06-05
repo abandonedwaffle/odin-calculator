7c6d9a791cbbc3c2ba9662dd691c92de4f18cd9f
Ritesh Diwan <riteshcph123@rediffmail.com>
2025-06-05 18:38:24 +0530

    added operator function

--- script.js
+++ script.js
@@ -20,4 +20,59 @@ let operator = null;
 
 
 const calculatorDisplay = document.getElementById("display");
-calculatorDisplay.textContent = 0;
+calculatorDisplay.textContent = null;
+
+const numButtons = document.querySelectorAll(".btn");
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const clickedNumber = button.dataset.number;
+
+        if (calculatorDisplay.textContent === '0') {
+            calculatorDisplay.textContent = clickedNumber;
+        } else {
+            calculatorDisplay.textContent += button.dataset.number;
+        }
+
+    })
+    
+})
+
+
+const operatorButtons = document.querySelectorAll(".btn-operator");
+
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const clickedOperator = button.dataset.operator;
+        if (clickedOperator === '=') {
+            operandTwo = calculatorDisplay.textContent;
+        } else {
+        operandOne = calculatorDisplay.textContent;
+        operator = clickedOperator;
+        calculatorDisplay.textContent = '0';
+        
+        }
+  
+    })
+    
+})
+
+
+function operate (operator, num1, num2) {
+    let result;
+    switch(operator) {
+        case '+':
+           result = addition(num1, num2);
+           return result;
+        case '-':
+            result =  subtraction(num1, num2);
+            return result;
+        case '*':
+            result =  multiplication(num1, num2);
+            return result;
+        case '/':
+            result =  division(num1, num2);
+            return result;            
+    }
+}
+
