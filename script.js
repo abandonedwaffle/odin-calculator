df46ee370c346aaba6acc8922e649d32ec9d39ad
Ritesh Diwan <riteshcph123@rediffmail.com>
2025-06-08 10:43:01 +0530

    final commit

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function addition (a, b) {
+function addition(a, b) {
     return a + b;
 }
 
-function subtraction (a, b) {
+function subtraction(a, b) {
     return a - b;
 }
 
-function multiplication (a, b) {
+function multiplication(a, b) {
     return a * b;
 }
 
-function division (a, b) {
+function division(a, b) {
     if (b === 0) {
         return "Can't divide by 0!"
     } else {
         return a / b;
     }
-    
+
 }
 
 let operandOne = 0;
@@ -44,7 +44,7 @@ numButtons.forEach(button => {
         }
 
     })
-    
+
 })
 
 
@@ -54,67 +54,70 @@ operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
         const clickedOperator = button.dataset.operator;
         if (clickedOperator === '=') {
-            operandTwo = calculatorDisplay.textContent;
-            let result = operate(operator, operandOne, operandTwo);
-            calculatorDisplay.textContent = result;
-            awaitingNextNumber = true;
-            operator = null;
-            operandOne = null;
-            operandTwo = null;
-
+            if (operator === null) {
+                calculatorDisplay.textContent = 0;
+            } else {
+                operandTwo = calculatorDisplay.textContent;
+                let result = operate(operator, operandOne, operandTwo);
+                calculatorDisplay.textContent = result;
+                awaitingNextNumber = true;
+                operator = null;
+                operandOne = null;
+                operandTwo = null;
+            }
         } else {
-            if (operator !== null && operandOne !== null) {
+            if ((operator !== null && operandOne !== null) && (calculatorDisplay.textContent !== '0') ) {
                 operandTwo = calculatorDisplay.textContent;
                 let result = operate(operator, operandOne, operandTwo)
                 operandOne = result;
                 calculatorDisplay.textContent = result;
             } else {
                 operandOne = calculatorDisplay.textContent;
-            } 
+            }
             operator = clickedOperator;
             calculatorDisplay.textContent = '0';
-        
+
         }
-  
+
     });
-    
+
 });
 
 
-function operate (operator, num1, num2) {
+function operate(operator, num1, num2) {
     let result;
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
-    switch(operator) {
+    switch (operator) {
         case '+':
-           result = addition(num1, num2);
-           result = parseFloat(result.toFixed(2));
-           return result;
+            result = addition(num1, num2);
+            result = parseFloat(result.toFixed(2));
+            return result;
         case '-':
-            result =  subtraction(num1, num2);
+            result = subtraction(num1, num2);
             result = parseFloat(result.toFixed(2));
             return result;
         case '*':
-            result =  multiplication(num1, num2);
+            result = multiplication(num1, num2);
             result = parseFloat(result.toFixed(2));
             return result;
         case '/':
-            result =  division(num1, num2);
+            result = division(num1, num2);
             if (typeof result === 'string') {
                 return result;
             } else {
                 result = parseFloat(result.toFixed(2));
                 return result;
             }
-                        
+
     }
 }
 
 const clearButton = document.querySelector(".btn-clear");
 
 clearButton.addEventListener("click", () => {
- operandOne = 0;
- operandTwo = 0;
- operator = null;
- calculatorDisplay.textContent = '0';
+    operandOne = 0;
+    operandTwo = 0;
+    operator = null;
+    calculatorDisplay.textContent = '0';
 })
